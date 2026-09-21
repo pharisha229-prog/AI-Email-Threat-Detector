afb908b712f72a9e6e89c849d135faa40b4581dd
Harisha <pharisha229@gmail.com>
2026-09-21 11:41:24 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -600,7 +600,4 @@ def file_too_large(error):
 # ==========================================
 
 if __name__ == "__main__":
-
-    app.run(
-        debug=False
-    )
+    app.run(host="0.0.0.0", port=5000, debug=False)
